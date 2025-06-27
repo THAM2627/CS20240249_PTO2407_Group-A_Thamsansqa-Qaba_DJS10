d09749271d9e0944f00258405770674499f0b644
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-06-27 13:42:01 +0200

    Update App.jsx

--- my-blog-app/src/App.jsx
+++ my-blog-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import fetchPosts from './components/fetchPosts'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -31,5 +32,7 @@ function App() {
     </>
   )
 }
+
+fetchPosts();
 
 export default App
